eecc8c0b533055c16d68327ffb92c9f5ecc02b82
Andre Luis Nascimento Araujo <luis.andre.8639@gmail.com>
2025-05-13 13:17:59 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "xtcrc7",
+  projectId: "osqv9y",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
